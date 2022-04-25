bb0673e836ee0813e31778dbc8b211fb6ff15a0d
Nikolay Lysak <Nikolay.Lysak@x5.ru>
2022-04-25 12:24:44 +0300

    [=] Fixtures updated

--- cypress/fixtures/todos-009/testData.ts
+++ cypress/fixtures/todos-009/testData.ts
@@ -1,9 +1,7 @@
-const listOfToDoEntries: string[] = [
+export const listOfToDoEntries: string[] = [
   'First ToDo record',
   'Second ToDo record',
   'Third ToDo record',
   'Fourth ToDo record',
   'Fifth ToDo record'
 ];
-
-export { listOfToDoEntries };
